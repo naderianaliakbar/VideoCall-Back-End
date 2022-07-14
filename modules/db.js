615085b3fac2ba5c3616c7c1a138d82b50cf0ccf
AliAkbar Naderian <naderianaliakbar@gmail.com>
2022-07-14 18:52:14 +0430

    change mongodb server

--- modules/db.js
+++ modules/db.js
@@ -1,11 +1,16 @@
-const MongoClient = require('mongodb').MongoClient;
-const url         = process.env.MongoDB_URL;
+const {MongoClient, ServerApiVersion} = require('mongodb');
+const url                             = process.env.MongoDB_URL;
 let _db;
 
 module.exports = {
-
     connect: function (callback) {
-        MongoClient.connect(url, function (err, client) {
+        const uri    = "mongodb+srv://exoroya:<f5lQmGwM1RjoAXAb>@exoroya.dq090yy.mongodb.net/?retryWrites=true&w=majority";
+        const client = new MongoClient(uri, {
+            useNewUrlParser   : true,
+            useUnifiedTopology: true,
+            serverApi         : ServerApiVersion.v1
+        });
+        client.connect(err => {
             _db = client.db(process.env.MongoDB_DATABASE);
             return callback(err);
         });
