3178c2ad1e6655705c22b17275893458fd8925f6
AliAkbar Naderian <naderianaliakbar@gmail.com>
2022-07-19 01:29:26 +0430

    add dist files to public #C7

---
+++ public/_nuxt/static/1658177934/call/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658177934",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fcall",config:{SOCKET_URL:"https:\u002F\u002Fexoroya.ir",PEER_URL:"peer.exoroya.ir",PEER_PORT:9000,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
